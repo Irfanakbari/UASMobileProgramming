70609560d4a1e7b17e6790c2621c34d46ccbf644
irfanakbari <irfanakbarihabibi18@gmail.com>
2023-01-13 16:10:37 +0700

    fix credit page

--- app/src/main/java/com/example/utsmobileprogramming/activity/CreditActivity.kt
+++ app/src/main/java/com/example/utsmobileprogramming/activity/CreditActivity.kt
@@ -1,12 +1,18 @@
 package com.example.utsmobileprogramming.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.utsmobileprogramming.BaseActivity
 import com.example.utsmobileprogramming.R
 
-class CreditActivity : AppCompatActivity() {
+class CreditActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_credit)
+        val backbutton = findViewById<TextView>(R.id.backmenu)
+
+        backbutton.setOnClickListener{
+            finish()
+        }
     }
 }
